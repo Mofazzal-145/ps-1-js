d8ed4ac2afe45b0dcb055212431f151085388416
Mofazzal-145 <md.mofazzalhossain145@gmail.com>
2022-01-31 15:02:07 +0600

    simple math calculation

--- addtion.js
+++
@@ -1,9 +0,0 @@
-// add three number 
-
-var number1 = 11;
-var number2 = 22;
-var number3 = 33;
-
-var addtion = number1 + number2 + number3;
-console.log(addtion);
-
---
+++ simple-math-operation.js
@@ -0,0 +1,32 @@
+// add three number 
+
+var number1 = 11;
+var number2 = 22;
+var number3 = 33;
+
+var addtion = number1 + number2 + number3;
+console.log('Addtion',addtion);
+
+// subtraction operation
+
+var subtraction = number3 - number2 - number1;
+console.log('subtraction',subtraction);
+
+
+// multiplication
+
+var mul = number1 * number2 * number3;
+console.log('multiplication',mul);
+
+
+// division
+
+var div = number3 / number2 / number1;
+console.log('division', div);
+
+
+
+
+
+
+
