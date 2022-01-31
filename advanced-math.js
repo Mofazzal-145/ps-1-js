b7294bf7572091b7055f94dbc3668dcb56a663bf
Mofazzal-145 <md.mofazzalhossain145@gmail.com>
2022-01-31 15:46:14 +0600

    another file added

---
+++ advanced-math.js
@@ -0,0 +1,21 @@
+var num1 = '11.22';
+var num2 = '33.32';
+
+// num1 = parseInt(num1);
+// num2 = parseInt(num2);
+
+// console.log(num1 + num2);
+
+num1 = parseFloat(num1);
+num2 = parseFloat(num2);
+
+console.log(num1 + num2);
+
+var firstNumber = 0.1;
+var secondNumber = 0.2;
+var total = firstNumber + secondNumber;
+total = total.toFixed(1);
+total =  parseFloat(total);
+console.log(total);
+// console.log(parseFloat(total.toFixed(1)));
+
