c8268ab555144edd3c9e85dc145c63d40e8b5e51
Mofazzal-145 <md.mofazzalhossain145@gmail.com>
2022-02-02 19:34:54 +0600

    grading system

---
+++ grading.js
@@ -0,0 +1,33 @@
+// grading system
+// let mark = 50;
+let mark = 94;
+// let mark = 70;
+// let mark = 80;
+// let mark = 90;
+
+if(mark < 50){
+    console.log('grade F');
+}
+else if(mark >=50 && mark < 60){
+    console.log('grade D')
+}
+else if(mark >=60 &&  mark < 70){
+    console.log('grade C')
+}
+else if(mark >=70 && mark < 80){
+    console.log('grade B')
+}
+else if(mark >=80 && mark < 90){
+    console.log('grade A')
+}
+else{
+    console.log('grade A+')
+}
+
+
+
+
+
+
+
+
