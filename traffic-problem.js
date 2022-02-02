78551ad5363ec5a8f9b30fee8d9ec938272c44af
Mofazzal-145 <md.mofazzalhossain145@gmail.com>
2022-02-02 20:03:10 +0600

    traffic signal system

---
+++ traffic-problem.js
@@ -0,0 +1,11 @@
+var signal = 'yellow';
+if(signal == 'red') {
+    console.log("It's danger");
+}
+else if(signal == 'yellow') {
+    console.log("I will stop");
+}
+else{
+    console.log('I will cross the road');
+}
+ 
